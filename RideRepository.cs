c4892aeec77e47c49acec9ef672fdcd47d5d39ef
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-18 13:29:08 +0530

    [Kalyan] added method for adding list of rides to a userid

---
+++ RideRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    class RideRepository
+    {
+        // Dictionary for storing the list of rides as value and userid as key
+        Dictionary<string, List<Ride>> userRides = null;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RideRepository"/> class.
+        /// </summary>
+        public RideRepository()
+        {
+            this.userRides = new Dictionary<string, List<Ride>>();
+        }
+        /// <summary>
+        /// Adds the ride to the dictionary 
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="rides">The rides.</param>
+        /// <exception cref="CabInvoiceGenerator.CabInvoiceException">Rides are null</exception>
+        public void AddRide(string userId, Ride[] rides)
+        {
+            // If userid does not exist in the dictionary the ridelist value
+            // will be false else true
+            bool rideList = this.userRides.ContainsKey(userId);
+            try
+            {
+                // If ridelist is false then the rides list will be added to dictionary
+                // With userid as key
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    // list for adding the different rides
+                    list.AddRange(rides);
+                    this.userRides.Add(userId, list);
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are null");
+            }
+        }
+        /// <summary>
+        /// Returns the rides as array of  the particulat userid passed else throws exception
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="CabInvoiceGenerator.CabInvoiceException">Invalid user ID</exception>
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch (Exception)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid user ID");
+            }
+        }
+    }
+}
